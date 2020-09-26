23ae22aae77f7f66e1799d319f88b3ff50c59929
Adrian <adrianivsdk@gmail.com>
2020-09-25 23:47:30 -0300

    This is a mess right now, don't worry

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Route, Link, NavLink, Switch, BrowserRouter } from 'react-router-dom'
+import Home from './components/Home'
+import Secret from './components/Secret'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import api from './api'
 
-function App() {
+function App(props) {
+  const [countries] = useState([])
+
+  function handleLogoutClick(e) {
+    api.logout()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <header className="App-header">
+          <h1 className="App-title">MERN Boilerplate</h1>
+          <NavLink to="/" exact>
+            Home
+          </NavLink>
+          <NavLink to="/countries">Countries</NavLink>
+          <NavLink to="/add-country">Add country</NavLink>
+          {!api.isLoggedIn() && <NavLink to="/signup">Signup</NavLink>}
+          {!api.isLoggedIn() && <NavLink to="/login">Login</NavLink>}
+          {api.isLoggedIn() && (
+            <Link to="/" onClick={e => handleLogoutClick(e)}>
+              Logout
+        </Link>
+          )}
+          <NavLink to="/secret">Secret</NavLink>
+        </header>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+          <Route path="/secret" component={Secret} />
+          <Route render={() => <h2>404</h2>} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
