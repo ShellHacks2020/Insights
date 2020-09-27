6e6ab05c768030c35fa3f4efce70e9496ab12dbb
Andres Weber <andresmweber@gmail.com>
2020-09-26 20:06:58 -0400

    Clarified debugs.

--- server/routes/tiktok.js
+++ server/routes/tiktok.js
@@ -7,15 +7,16 @@ const logger = debug("insights:tiktok");
 router.get("/", async (req, res, next) => {
   try {
     const hashtagPosts = await getHashtag(req.query.search || "dance");
-    logger(`Received ${hashtagPosts.length} hashtag posts!`);
+    logger(`Top ${hashtagPosts.length} TikToks for #${req.query.search}`);
     const users = await Promise.all(
-      hashtagPosts.map((post) => getUserInfo(post.authorMeta.name))
+      hashtagPosts.map((post) =>
+        getUserInfo(post.authorMeta.name).catch((err) => err)
+      )
     );
     logger(users);
     res.json(users);
   } catch (error) {
-    logger(error);
-    next(error);
+    logger(`Ran into error getting tiktok hashtags: %o`, error);
   }
 });
 
